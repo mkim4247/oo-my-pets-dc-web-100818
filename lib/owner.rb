ed5317fa25199fc2ecca34d9a6ab0191d147e709
Michael Kim <m.kim4247@gmail.com>
2020-10-24 19:22:37 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -55,7 +55,7 @@ class Owner
   end
   
   def sell_pets
-    self.pets.each { |type, pets| 
+    self.pets.collect { |type, pets| 
     pets.collect { |attribute| attribute.mood == "nervous" }}
     self.pets.each { |type, pets| pets.clear }
   end
